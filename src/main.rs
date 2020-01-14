5a49c0e2cad78d7f21c3aa0bee47da7b194f35bc
zhijazi <hijazizein2014@gmail.com>
2020-01-13 20:01:31 -0500

    Several structural changes
    Uses linux metadata api, removed return type on main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 extern crate getopts;
+extern crate chrono;
+extern crate users;
 
 use std::{ env, fs };
-use std::os::unix::fs::PermissionsExt;
+use std::os::linux::fs::MetadataExt;
+
 use getopts::Options;
 
-fn main() -> std::io::Result<()>{
+use chrono::offset::Local;
+use chrono::DateTime;
+
+use users::{get_user_by_uid};
+
+fn main() {
     let args: Vec<String> = env::args().collect();
 
     let mut opts = Options::new();
@@ -22,12 +30,16 @@ fn main() -> std::io::Result<()>{
     };
 
     if arg_matches.opt_present("l") {
-        output_dir_detailed(&path[..]);
+        match output_dir_detailed(&path[..]) {
+            Ok(_) => (),
+            Err(e) => panic!("{}", e.to_string())
+        };
     } else {
-        output_dir_contents(&path[..]);
+        match output_dir_contents(&path[..]) {
+            Ok(_) => (),
+            Err(e) => panic!("{}", e.to_string())
+        };
     }
-
-    Ok(())
 }
 
 fn output_dir_contents(path: &str) -> std::io::Result<()> {
@@ -42,7 +54,22 @@ fn output_dir_detailed(path: &str) -> std::io::Result<()> {
     for entry in fs::read_dir(path)? {
         let file = entry?;
         let metadata = file.metadata()?;
-        println!("{:?} {} {} {} {} {:?} {:?}", metadata.permissions().mode(), 1, "zein", "zein", metadata.len(), metadata.modified(), file.file_name());
+
+        let user = match get_user_by_uid(metadata.st_uid()) {
+            Some(x) => x,
+            None => panic!("Could not find user")
+        };
+
+        let group = match get_user_by_uid(metadata.st_gid()) {
+            Some(x) => x,
+            None => panic!("Could not find group")
+        };
+
+        let last_modified = metadata.modified()?;
+        let last_modified: DateTime<Local> = last_modified.into();
+
+        println!("<perms> {} {} {} {} {} {:?}",metadata.st_nlink(), user.name().to_string_lossy(), group.name().to_string_lossy(), metadata.len(), last_modified.format("%b %d %H:%M"), file.file_name());
     }
     Ok(())
 }
+
